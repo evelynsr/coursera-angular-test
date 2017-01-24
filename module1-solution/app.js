b40f42df05efdd57762de5fae62ac8df8a501c68
evelynsr <saroev24@gmail.com>
2017-01-24 17:16:32 -0600

    finished homework

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,14 +7,31 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 $scope.items = "";
+$scope.message = "";
+$scope.obj = "";
+var cantidad = 0;
   $scope.getWordCounter = function() {
-    var cantidad = $scope.items.trim().split(',').length;
-    if(cantidad<=3){
-      return "Enjoy";
-    }else if (cantidad>3) {
-      return "Too much!";
-    }
 
+cantidad = $scope.items.trim();
+if(cantidad.length===0){
+  $scope.message = "Please enter data first";
+  $scope.obj = {
+    "color" : "red"
+  }
+}else{
+  cantidad = $scope.items.split(',').length;
+  if(cantidad>=1&&cantidad<=3){
+    $scope.message = "Enjoy!";
+    $scope.obj = {
+      "color" : "green"
+    }
+  }else{
+    $scope.message = "Too much!";
+    $scope.obj = {
+      "color" : "green"
+    }
+  }
+}
 }
 
 }
